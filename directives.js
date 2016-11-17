ecf91257c116ebea5998cd5cd80483d3dbc3a04c
durainm <durainm@gmail.com>
2016-11-17 11:16:34 +0530

    directives js
    Directives life cycle

--- directives.js
+++ directives.js
@@ -8,9 +8,11 @@ In a directive's life cycle, there are four distinct functions that can execute 
 Each enables the developer to control and customize the directive at different points of the life cycle.
 The four functions are: compile, controller, pre-link and post-Link.
 
-The compile function allows the directive to manipulate the DOM before it is compiled and linked thereby allowing it to add/remove/change directives, as well as, add/remove/change other DOM elements.
+The compile function allows the directive to manipulate the DOM before it is compiled and linked thereby allowing it to 
+add/remove/change directives, as well as, add/remove/change other DOM elements.
 
-The controller function facilitates directive communication. Sibling and child directives can request the controller of their siblings and parents to communicate information.
+The controller function facilitates directive communication. Sibling and child directives 
+can request the controller of their siblings and parents to communicate information.
 
 The pre-link function allows for private $scope manipulation before the post-link process begins.
 
